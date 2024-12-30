4686b90347788834e9f924acda69eecdbdbbd7ea
spacekimchi <spacekimchis@gmail.com>
2024-12-29 23:43:46 -0800

    tmp fix

--- app/services/tmp.rb
+++ app/services/tmp.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 class Tmp
   def self.translate_passages
-    languages = ["Spanish (Spain)", "Spanish (Latin America)", "Korean", "Japanese", "Hindi", "French", "Italian", "Portuguese", "German", "Russian", "Mandarin", "Arabic", "Vietnamese", "Cantonese"]
+    languages = ["Spanish (Spain)", "Spanish (Latin America)", "Korean", "Japanese", "Hindi", "French", "Italian", "Portuguese", "German", "Russian", "Mandarin", "Vietnamese", "Cantonese"]
     passages = Passage.all
     passages.each do |passage|
       languages.each do |lang|
