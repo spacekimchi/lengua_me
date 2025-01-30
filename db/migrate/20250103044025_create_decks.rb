56f3b80665d1a216bfae62699e4ed200077dd97f
spacekimchi <spacekimchis@gmail.com>
2025-01-30 15:06:14 -0800

    wip

---
+++ db/migrate/20250103044025_create_decks.rb
@@ -0,0 +1,11 @@
+class CreateDecks < ActiveRecord::Migration[8.0]
+  def change
+    create_table :decks, id: :uuid do |t|
+      t.references :user, type: :uuid, foreign_key: true, null: false
+      t.text :name
+      t.timestamps
+    end
+
+    add_index :decks, [:user_id, :name], unique: true
+  end
+end
---
+++ spec/factories/flashcards.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :flashcard do
+    
+  end
+end
