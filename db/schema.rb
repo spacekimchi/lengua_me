8937406659c981308ed03893eb299070ee4a381f
spacekimchi <spacekimchis@gmail.com>
2025-01-30 15:51:08 -0800

    migration for unique user and deck per card

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_03_044225) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_30_235017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -93,6 +93,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_03_044225) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["deck_id"], name: "index_flashcards_on_deck_id"
+    t.index ["user_id", "deck_id"], name: "index_flashcards_on_user_id_and_deck_id", unique: true
     t.index ["user_id"], name: "index_flashcards_on_user_id"
   end
 
