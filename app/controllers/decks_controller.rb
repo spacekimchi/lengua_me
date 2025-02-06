cc675d709983d72bdc992522838a73bf24d7ee7a
spacekimchi <spacekimchis@gmail.com>
2025-02-05 19:19:23 -0800

    flash cards deck creation

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -3,6 +3,18 @@ class DecksController < ApplicationController
 
   def create
     @deck = current_user.decks.new(deck_params)
+    respond_to do |format|
+      if @deck.save
+        flash[:success] = "Successfully created a new deck."
+        format.turbo_stream
+        format.html { redirect_to decks_path, notice: "Deck created." }
+      else
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("deck_form", partial: "flashcards/add_deck_form")
+        end
+        format.html { render :new }
+      end
+    end
   end
 
   private
